22fd321d4f42d554188d295808696aa17f783868
lanmx <lanmx@1391.com>
2019-07-25 14:32:50 +0800

    测试并发两个请求

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -1,20 +1,21 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_elm/utils/dio_util.dart';
 import 'package:dio/dio.dart';
 
 class SecondPage extends StatelessWidget {
-
   final String title;
 
-  SecondPage({Key key, this.title = "嘿嘿"}): super(key: key);
+  SecondPage({Key key, this.title = "嘿嘿"}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    _getHttp().then((data) {
-    // print('Http response: $data');
-  });
+    _getAll().then((data) {
+      print('Http response: ${data[0]['d'][0]['content']}');
+    }).catchError((onError) {
+      print("出现错误！！！");
+      print(onError);
+    });
+
     return Scaffold(
       backgroundColor: Colors.pinkAccent,
       appBar: AppBar(
@@ -38,15 +39,29 @@ class SecondPage extends StatelessWidget {
     );
   }
 
-  _getHttp() async {
-    
+  Future _getHttp() async {
     var dio = DioUtil.http();
-    
-    Response<Map<String, dynamic>> response = await dio.get("/activity_link.json");
-    print(response.data);
 
-    return response;
-    
+    Response<Map<String, dynamic>> response =
+        await dio.get("/activity_link.json");
+
+    return response.data;
+  }
+
+  Future _getHttp1() async {
+    var dio = DioUtil.http();
+
+    Response<Map<String, dynamic>> response =
+        await dio.get("/activity_link.json");
+
+    return response.data;
+  }
+
+  Future _getAll() async {
+  
+    List<dynamic> responses = await Future.wait([_getHttp(), _getHttp1()]);
+
+    return responses;
   }
 
 }
